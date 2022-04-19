84fabfe4a22f74afa9d74bcf8a626841d5037dd6
rgestudillo <90782009+rgestudillo@users.noreply.github.com>
2022-04-20 02:03:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,9 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Affinity Guild" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Coming soon
         </p>
       </main>
 
